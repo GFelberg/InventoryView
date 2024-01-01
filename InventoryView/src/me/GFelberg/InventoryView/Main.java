913a98abe273bf5de3cbc3920fed5d2be5006d0d
Gabriel M. Felberg <gabrielmfelberg@gmail.com>
2024-01-01 02:37:12 -0300

    Updating

--- InventoryView/src/me/GFelberg/InventoryView/Main.java
+++ InventoryView/src/me/GFelberg/InventoryView/Main.java
@@ -1,6 +1,11 @@
 package me.GFelberg.InventoryView;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.GFelberg.InventoryView.commands.Invechest;
@@ -9,8 +14,9 @@ import me.GFelberg.InventoryView.commands.Invsee;
 import me.GFelberg.InventoryView.data.InventorySystem;
 import me.GFelberg.InventoryView.events.InventoryEvents;
 import me.GFelberg.InventoryView.utils.InventoryViewUtils;
+import me.GFelberg.InventoryView.utils.UpdateChecker;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
 	private static Main instance;
 
@@ -19,11 +25,19 @@ public class Main extends JavaPlugin {
 		saveDefaultConfig();
 		loadCommands();
 		loadVariables();
-		Bukkit.getPluginManager().registerEvents(new InventoryEvents(), this);
+		loadEvents();
 		Bukkit.getConsoleSender().sendMessage("-----------------------------");
 		Bukkit.getConsoleSender().sendMessage("InventoryView Plugin Enabled!");
 		Bukkit.getConsoleSender().sendMessage("Plugin developed by GFelberg");
 		Bukkit.getConsoleSender().sendMessage("-----------------------------");
+
+		new UpdateChecker(this, 75100).getVersion(version -> {
+			if (this.getDescription().getVersion().equals(version)) {
+				getLogger().info("There is not a new update available.");
+			} else {
+				getLogger().info("There is a new update available.");
+			}
+		});
 	}
 
 	public static Main getInstance() {
@@ -46,5 +60,25 @@ public class Main extends JavaPlugin {
 	public void loadVariables() {
 		InventoryViewUtils.loadVariables();
 		InventorySystem.loadVariables();
+	}
+
+	public void loadEvents() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents(new InventoryEvents(), this);
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player p = event.getPlayer();
+		FileConfiguration config = this.getConfig();
+
+		if (config.getBoolean("update-check") && p.isOp()
+				|| config.getBoolean("update-check") && p.hasPermission("inventory.update")) {
+			(new UpdateChecker(this, 75100)).getVersion((version) -> {
+				if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
+					p.sendMessage("There is a new update available of InventoryView.");
+				}
+			});
+		}
 	}
 }
--- InventoryView/src/me/GFelberg/InventoryView/data/InventorySystem.java
+++ InventoryView/src/me/GFelberg/InventoryView/data/InventorySystem.java
@@ -10,7 +10,7 @@ public class InventorySystem {
 
 	public static String player_inv, player_inv_self, player_notfound;
 	public static String player_echest, player_echest_self;
-	public static volatile String player_inv_name, player_echest_name;
+	public static volatile String player_inv_name = "Test1", player_echest_name = "Test2";
 
 	public static void loadVariables() {
 		player_inv = Main.getInstance().getConfig().getString("PlayerInventory.Message").replace("&", "§");
@@ -27,16 +27,16 @@ public class InventorySystem {
 		if (selected == null) {
 			p.sendMessage(player_notfound);
 			return;
-			// } else if (selected == p) {
-			// p.sendMessage(player_inv_self);
-			// return;
+		} else if (selected == p) {
+			p.sendMessage(player_inv_self);
+			return;
 		} else {
 			Inventory inv = selected.getInventory();
 			String player_inv_title = "%player_inv_title%";
-			Inventory updatedInventory = Bukkit.createInventory(null, inv.getType(),
+			Inventory updatedInventory = Bukkit.createInventory(selected, inv.getType(),
 					player_inv_title.replace("%player_inv_title%", selected.getName() + " - Inventory"));
 			updatedInventory.setContents(inv.getContents());
-			selected.openInventory(updatedInventory);
+			p.openInventory(updatedInventory);
 			player_inv_name = selected.getName();
 		}
 		p.sendMessage(player_inv.replace("%player_inv%", selected.getName()));
@@ -47,16 +47,16 @@ public class InventorySystem {
 		if (selected == null) {
 			p.sendMessage(player_notfound);
 			return;
-			// } else if (selected == p) {
-			// p.sendMessage(player_echest_self);
-			// return;
+		} else if (selected == p) {
+			p.sendMessage(player_echest_self);
+			return;
 		} else {
 			Inventory inv = selected.getEnderChest();
 			String player_echest_title = "%player_echest_title%";
-			Inventory updatedInventory = Bukkit.createInventory(null, inv.getType(),
+			Inventory updatedInventory = Bukkit.createInventory(selected, inv.getType(),
 					player_echest_title.replace("%player_echest_title%", selected.getName()) + " - Enderchest");
 			updatedInventory.setContents(inv.getContents());
-			selected.openInventory(updatedInventory);
+			p.openInventory(updatedInventory);
 			player_echest_name = selected.getName();
 		}
 		p.sendMessage(player_echest.replace("%player_echest%", selected.getName()));
